99dee44dd2c9d80658fef2e56b761f15bce77125
Alejandro <aleperaltabazas@hotmail.com>
2019-10-27 21:12:34 -0300

    add test for Annotation.write

--- src/main/scala/uml/model/annotations/Annotation.scala
+++ src/main/scala/uml/model/annotations/Annotation.scala
@@ -1,7 +1,7 @@
 package uml.model.annotations
 
 case class Annotation(name: String, properties: Map[String, String]) {
-  def write: String = s"$name($formatProperties)"
+  def write: String = s"@$name($formatProperties)"
 
   private def formatProperties = s"${
     properties.toList.map { prop =>
